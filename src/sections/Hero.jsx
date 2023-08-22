4c6f559c2bedf41db6aaf729bb495bd44f3beee4
Erudite885 <erudite885@gmail.com>
2023-08-22 14:10:25 +0100

    hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import { useState } from "react";
+import { shoes, statistics } from "../constants";
+import { Button, ShoeCard } from "../components";
+import { bigShoe1 } from "../assets/images";
+import { arrowRight } from "../assets/icons";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const [bigShoeImage, setBigShoeImage] = useState(bigShoe1);
 
-export default Hero
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collections
+        </p>
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold ">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="mt-3 inline-block text-coral-red">Nike</span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm ">
+          Discover the stylish Nike Arrivals, quality Comfort, and Innovation
+          for your active life.
+        </p>
+        <Button label="Shop now" iconUrl={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat, index) => (
+            <div key={index}>
+              <p className="font-palanquin text-4xl font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative justify-center items-start flex flex-1 xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoeImage}
+          alt="shoe collection"
+          width={610}
+          height={502}
+          className="relative z-10 object-contain"
+        />
+
+        <div className="flex gap-4 sm:gap-6 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6 ">
+          {shoes.map((image, index) => (
+            <div key={index}>
+              <ShoeCard
+                index={index}
+                imgUrl={image}
+                changeBigShoeImage={(shoe) => setBigShoeImage(shoe)}
+                bigShoeImage={bigShoeImage}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
