e8cc82372a10260793449905bdb7becb9a03eef1
Erudite885 <erudite885@gmail.com>
2023-08-23 10:35:46 +0100

    constants adjustments fixed

--- src/constants/index.js
+++ src/constants/index.js
@@ -44,31 +44,31 @@ export const shoes = [
 ];
 
 export const statistics = [
-  { value: "1k+", label: "Brands" },
-  { value: "500+", label: "Shops" },
-  { value: "250k+", label: "Customers" },
+  { value: "2k+", label: "Brands" },
+  { value: "350+", label: "Shops" },
+  { value: "650k+", label: "Customers" },
 ];
 
 export const products = [
   {
     imgURL: shoe4,
     name: "Nike Air Jordan-01",
-    price: "$200.20",
+    price: "$210.25",
   },
   {
     imgURL: shoe5,
     name: "Nike Air Jordan-10",
-    price: "$210.20",
+    price: "$225.30",
   },
   {
     imgURL: shoe6,
     name: "Nike Air Jordan-100",
-    price: "$220.20",
+    price: "$235.25",
   },
   {
     imgURL: shoe7,
     name: "Nike Air Jordan-001",
-    price: "$230.20",
+    price: "$250.75",
   },
 ];
 
