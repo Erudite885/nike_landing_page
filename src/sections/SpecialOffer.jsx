3fddbf1e2fa524bc2f1db0e45604c55e27da584f
Erudite885 <erudite885@gmail.com>
2023-08-23 01:34:08 +0100

    specialoffer section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import { Button } from "../components";
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <section className="flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="Shoe promotion"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="text-4xl font-bold font-palanquin">
+          <span className="text-coral-red">Special</span> Offer
+        </h2>
+        <p className="info-text mt-4">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incresible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="info-text mt-6">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop now" iconURL={arrowRight} />
+          <Button
+            label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
